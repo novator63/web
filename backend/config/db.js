4c7cd754ab2d51b4d76c2026c6e58900e97b8912
novator63 <pachk.kir@gmail.com>
2026-03-14 18:04:11 +0400

    feat:lab1 implement database connection with Sequelize and update index.js

---
+++ backend/config/db.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";  //3.2 импортировать Sequelize из пакета sequelize
+import dotenv from "dotenv";    //3.3 импортировать dotenv для загрузки переменных окружения из файла .env
+
+dotenv.config();
+
+const sequelize = new Sequelize(    //3.4 создать объект sequelize с параметрами подключения
+  process.env.DB_NAME,       // Имя базы данных
+  process.env.DB_USER,       // Имя пользователя
+    process.env.DB_PASSWORD,   // Пароль
+    {
+        host: process.env.DB_HOST,   // Хост базы данных
+        port: process.env.DB_PORT,   // Порт базы данных
+        dialect: 'postgres',         // Диалект базы данных
+    }
+);
+
+export const authenticate = async () => {   //3.5 создать функцию authenticate для проверки подключения к базе данных
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to the database has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+export default sequelize;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import sequelize from './config/db.js'; //4.1 Импортировать sequelize из db.js
 
 dotenv.config();
 
@@ -14,6 +15,14 @@ const PORT = process.env.PORT || 5000;      //2.3 определить порт,
 app.get('/', (req, res) => {                //2.4 добавить тестовый маршрут GET /, который возвращает JSON с сообщением "Hello, World!"
   res.json({ message: 'Hello, World!' });   
 });
+
+sequelize.authenticate()  //4.1	вызвать sequelize.authenticate(), чтобы убедиться, что соединение установлено
+  .then(() => {
+    console.log('Database connection has been established successfully.');  // 4.1 вывести сообщение в консоль при успешном подключении
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 app.listen(PORT, () => {                              //3.1 настроить прослушивание сервера на указанном порту
   console.log(`Server is running on port ${PORT}`);
